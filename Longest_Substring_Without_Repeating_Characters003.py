876da7d9546518b7d0aa8b3d8aa3ea13c2781492
Wang Fengning <17749734946@163.com>
2016-12-25 12:59:09 +0800

    003

---
+++ Longest_Substring_Without_Repeating_Characters003.py
@@ -0,0 +1,29 @@
+# coding:utf-8
+__author__ = '王丰宁'
+'''
+https://leetcode.com/articles/longest-substring-without-repeating-characters/
+Given a string, find the length of the longest substring without repeating characters.
+'''
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        lstsstr = ''
+        left = 0
+        cache = {}
+        for index,value in enumerate(s):
+            if value in cache and cache[value] >= left:
+                left = cache[value] + 1
+            cache[value] = index
+            if len(lstsstr) < index - left + 1 :
+                lstsstr = s[left:index+1]
+        return lstsstr
+
+
+if __name__ == '__main__':
+    s = Solution()
+    l = 'pwwkew'
+    print s.lengthOfLongestSubstring(l)
+
